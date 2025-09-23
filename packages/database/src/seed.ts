f352b8831025b39d6872363a23b0193b5f4b563f
Paul Healy <paul156h@gmail.com>
2025-09-23 17:18:43 -0400

    Added data in json files

--- packages/database/src/seed.ts
+++ packages/database/src/seed.ts
@@ -1,34 +1,80 @@
 import { prisma } from "./client";
 
-import type { User } from "../generated/client";
+import users from "./seed-data/users.json";
+import courses from "./seed-data/courses.json";
+import enrollments from "./seed-data/enrollments.json";
+import feedbacks from "./seed-data/feedbacks.json";
+import grades from "./seed-data/grades.json";
+import submissions from "./seed-data/submissions.json";
+import assignments from "./seed-data/assignments.json";
 
-const DEFAULT_USERS = [
-  // Add your own user to pre-populate the database with
-  {
-    name: "Tim Apple",
-    email: "tim@apple.com",
-  },
-] as Array<Partial<User>>;
 
 (async () => {
   try {
     await Promise.all(
-      DEFAULT_USERS.map((user) =>
+      users.map((user) =>
         prisma.user.upsert({
-          where: {
-            email: user.email!,
-          },
-          update: {
-            ...user,
-          },
-          create: {
-            ...user,
-          },
+          where: { email: user.email! }, // email is unique in schema
+          update: { ...user },
+          create: { ...user, emailVerified: null },
         })
       )
     );
+
+    await Promise.all(
+      courses.map((course) =>
+        prisma.course.upsert({
+          where: { id: course.id! },
+          update: { ...course },
+          create: { ...course },
+        })
+      )
+    );
+
+    await Promise.all(
+      enrollments.map((enrollment) =>
+        prisma.course.upsert({
+          where: { id: enrollment.id! },
+          update: { ...enrollment },
+          create: { ...enrollment },
+        })
+      )
+    );
+
+    await Promise.all(
+      feedbacks.map((feedback) =>
+        prisma.course.upsert({
+          where: { id: feedback.id! },
+          update: { ...feedback },
+          create: { ...feedback },
+        })
+      )
+    );
+
+    await Promise.all(
+      grades.map((grades) =>
+        prisma.course.upsert({
+          where: { id: grades.id! },
+          update: { ...grades },
+          create: { ...grades },
+        })
+      )
+    );
+    
+    await Promise.all(
+      submissions.map((submissions) =>
+        prisma.course.upsert({
+          where: { id: submissions.id! },
+          update: { ...submissions },
+          create: { ...submissions },
+        })
+      )
+    );
+
+
+    console.log("✅ Database seeded with users and courses");
   } catch (error) {
-    console.error(error);
+    console.error("❌ Seeding failed:", error);
     process.exit(1);
   } finally {
     await prisma.$disconnect();
