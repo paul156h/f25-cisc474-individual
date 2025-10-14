008cfdf002e20b324bf5468b6a823016c442b34c
Paul Healy <paul156h@gmail.com>
2025-10-13 20:32:30 -0400

    More cors problems

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,7 +7,10 @@ import { appendFile } from 'fs';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ["http://localhost:3001", "https://cisc474-individual.paul156h.workers.dev/assignments"]
+    origin: [
+      "http://localhost:3001", 
+      "https://cisc474-individual.paul156h.workers.dev",
+    ],
   });
   const port = 3000;
   const host = process.env.HOST || '0.0.0.0';
